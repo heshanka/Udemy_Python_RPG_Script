f5ca08da59ac662778a09b6f9b9753eba13225bb
coderHHH <heshanka123@gmail.com>
2018-10-15 13:06:28 +0530

    reduce inventory when used

--- battle/main.py
+++ battle/main.py
@@ -22,7 +22,7 @@ hielixer = Item("MegaElixer", "elixer", "Fully restores HP/MP of one party membe
 grenade = Item("Grenade", "attack", "Deals 500 damage", 500)
 
 player_spells = [fire, thunder, blizzard, meteor, quake, cure, cura]
-player_items = [potion, hipotion, superpotion, elixer, hielixer, grenade]
+player_items = [{"item": potion, "quantity":15}, {"item":hipotion, "quantity":5}, {"item":superpotion, "quantity":5},{"item":elixer, "quantity":5}, {"item":hielixer, "quantity":2}, {"item":grenade, "quantity":5}]
 
 
 player = Person(460, 65, 60, 34, player_spells, player_items)
@@ -81,7 +81,13 @@ while running:
         if item_choice == -1:
             continue
 
-        item = player.items[item_choice]
+        item = player.items[item_choice]["item"]
+
+        if  player.items[item_choice]["quantity"] == 0:
+            print(bcolors.FAIL + "\n" + "None left..." + bcolors.ENDC)
+            continue
+        player.items[item_choice]["quantity"] -= 1
+
 
         if item.type == "potion":
             player.heal(item.prop)
